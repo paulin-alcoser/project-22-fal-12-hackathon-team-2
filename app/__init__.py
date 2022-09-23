afb547986aec726519d77d57134f1754da24de80
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2022-09-23 10:24:43 -0700

    Added json

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,9 +16,14 @@ def base():
 
 @app.route('/home/<username>')
 def about(username):
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     if username == "PaulinAlcoser":
-        return render_template('about.html')
+        json_url = os.path.join(SITE_ROOT, "static/data", "Paulin.json")
+        data = json.load(open(json_url))
+        return render_template('about.html', data=data)
     elif username == "AlexisGray":
-        return render_template('about.html')
+        json_url = os.path.join(SITE_ROOT, "static/data", "Alexis.json")
+        data = json.load(open(json_url))
+        return render_template('about.html', data=data)
     else: 
         return "<h1>Username did not match</h1>"
