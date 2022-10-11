166219242b5235ce3496c2e741cc339fc88637e0
paulin-alcoser <pac469@nyu.edu>
2022-10-10 17:34:14 -0700

    fix redeploy

--- app/redeploy-script.sh
+++ app/redeploy-script.sh
@@ -6,8 +6,6 @@ cd /root/project-22-fal-12-hackathon-team-2
 
 git fetch && git reset origin/main --hard
 
-# Install python virtual environment
-python -m venv python3-virtualenv
  
 # Enter python virtual environment
 source python3-virtualenv/bin/activate
@@ -16,5 +14,5 @@ source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
 tmux new -d -s my_session
-tmux send-keys -t my_session "source python3-virtualenv/bin/activate" Enter
+
 tmux send-keys -t my_session "flask run --host=137.184.86.159" Enter
