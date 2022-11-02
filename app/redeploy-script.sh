cdf5c9aafc6faed4506b378c5cb24dd60911871d
paulin-alcoser <pac469@nyu.edu>
2022-11-02 14:30:49 -0700

    added redeploy

--- app/redeploy-script.sh
+++ app/redeploy-script.sh
@@ -4,13 +4,6 @@ cd project-22-fal-12-hackathon-team-2
 
 git fetch && git reset origin/main --hard
 
-# Install python virtual environment
-python -m venv python3-virtualenv
+docker compose -f docker-compose.prod.yml down
 
-# Enter python virtual environment
-source python3-virtualenv/bin/activate
-
-#install python dependencies
-pip install -r requirements.txt
-
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
